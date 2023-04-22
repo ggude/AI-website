ce48bfc0ff13f9ab7bb8c3bdc0f4c80859055796
gude <gnapika1@gmail.com>
2023-04-22 20:02:50 +0530

    modified

--- python_website/app.py
+++ python_website/app.py
@@ -1,4 +1,5 @@
-import boto3
+import requests
+import json
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -7,41 +8,34 @@ app = Flask(__name__)
 API_GATEWAY_REGION = 'ap-south-1'  # Mumbai region
 API_GATEWAY_URL = 'https://pxxzbut94j.execute-api.ap-south-1.amazonaws.com/prod/api-gpt-ml'
 
-# Initialize the API Gateway client
-client = boto3.client('apigateway', region_name=API_GATEWAY_REGION)
-
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    response_text = ""
+    generated_text = ""
     if request.method == 'POST':
         # Get the text input from the form
         text_input = request.form['text_input']
 
         # Prepare the API Gateway request
         api_request = {
-            "method": "POST",
-            "url": API_GATEWAY_URL,
-            "headers": {
-                "Content-Type": "application/json"
-            },
-            "body": {
-                "input": text_input
-            }
+            "input": text_input
         }
 
         # Call the API Gateway
-        response = client.test_invoke_method(restApiId='your-api-gateway-id',
-                                             resourceId='your-resource-id',
-                                             httpMethod='POST',
-                                             body=api_request)
-        if response['statusCode'] == 200:
-            response_text = response['body']
+        response = requests.post(API_GATEWAY_URL, json=api_request)
+        if response.status_code == 200:
+            response_text = response.text
+            response_dict = json.loads(response_text)
+            if 'output' in response_dict.keys():
+                output = response_dict['output']
+                response_string = output.replace("'", "\"")
+                response_list = json.loads(response_string)
+                generated_text = response_list[0]['generated_text']
+            else:
+                generated_text = "Error: 'generated_text' key not found in API response"
         else:
-            response_text = f"Error: {response['statusCode']}"
+            generated_text = f"Error: {response.status_code}"
 
-    return render_template('index.html', response_text=response_text)
-
+    return render_template('index.html', generated_text=generated_text, css_file='style.css')
 
 if __name__ == '__main__':
     app.run(debug=True)
